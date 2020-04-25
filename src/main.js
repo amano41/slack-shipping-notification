d720c43d9d65c0c33f176dc715588bebce79ac83
Amano, Yoichi <amano.yoichi@gmail.com>
2020-04-25 11:33:05 +0900

    Rename notifyShipping() to main()

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-function notifyShipping() {
-  
+function main() {
+
   var properties = PropertiesService.getScriptProperties().getProperties();
   var query = properties.SEARCH_QUERY;
   var amazon = /amazon.co.jp/;
-  
+
   var threads = GmailApp.search(query);
-  
+
   for (var i in threads) {
-    
+
     var thread = threads[i];
     var messages = thread.getMessages();
-    
+
     // スレッド内のメッセージを 1 件ずつ処理
     for (var j in messages) {
-      
+
       var message = messages[j];
-      
+
       // 既読ならスキップ
       if (!message.isUnread()) {
         Logger.log("Skipped: " + message.getSubject());
@@ -25,7 +25,7 @@ function notifyShipping() {
       var from = message.getFrom();
       var subject = message.getSubject();
       var body = message.getPlainBody();
-      
+
       // Amazon.co.jp からの配送
       if (from.match(amazon)) {
         postAmazonNotification(subject, body);
@@ -33,25 +33,25 @@ function notifyShipping() {
         message.refresh();  // 既読を反映
         continue;
       }
-      
+
     }
 
     // スレッドを最新の状態に更新
     // これを実行しないと既読が反映されない
     thread.refresh();
-    
+
     // メッセージがすべて既読ならアーカイブ
     if (!thread.isUnread()) {
       thread.moveToArchive();
     }
-    
+
   }
-    
+
 }
 
 
 function postNotification(notifications) {
-  
+
   var channel = "#general";
   var username = "配達予定";
   var icon_emoji = ":package:";
@@ -61,7 +61,7 @@ function postNotification(notifications) {
 
 
 function createNotification(title, link, date, carrier, slipNumber, orderDetail) {
-  
+
   return {
     "title" : title,
     "title_link" : link,
@@ -74,14 +74,14 @@ function createNotification(title, link, date, carrier, slipNumber, orderDetail)
   };
 
 }
-          
-          
+
+
 function createField(title, value, short) {
-  
+
   return {
     "title" : title,
     "value" : value,
     "short" : short
   };
-  
+
 }
